1c8d9e8f90b03be950b447a640537535297d8664
samme <samme@users.noreply.github.com>
2018-02-13 15:30:56 -0800

    Try scene from ES6 class
    <http://labs.phaser.io/edit.html?src=src\scenes\scene%20from%20es6%20class.js>

--- app/initialize.js
+++ app/initialize.js
@@ -1,3 +1,34 @@
+class Scene extends Phaser.Scene {
+
+  constructor (sceneConfig) {
+    super(sceneConfig);
+  }
+
+  preload () {
+    this.load.setPath('assets/');
+    this.load.image('sky', 'space3.png');
+    this.load.image('logo', 'phaser3-logo.png');
+    this.load.image('red', 'red.png');
+  }
+
+  create () {
+    const sky = this.add.image(400, 300, 'sky');
+    sky.alpha = 0.5;
+    const particles = this.add.particles('red');
+    const emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+    });
+    const logo = this.physics.add.image(400, 100, 'logo');
+    logo.setVelocity(100, 200);
+    logo.setBounce(1, 1);
+    logo.setCollideWorldBounds(true);
+    emitter.startFollow(logo);
+  }
+
+}
+
 var config = {
 
   type: Phaser.AUTO,
@@ -13,32 +44,7 @@ var config = {
     }
   },
 
-  scene: {
-
-    preload() {
-      this.load.setPath('assets/');
-      this.load.image('sky', 'space3.png');
-      this.load.image('logo', 'phaser3-logo.png');
-      this.load.image('red', 'red.png');
-    },
-
-    create() {
-      const sky = this.add.image(400, 300, 'sky');
-      sky.alpha = 0.5;
-      const particles = this.add.particles('red');
-      const emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-      });
-      const logo = this.physics.add.image(400, 100, 'logo');
-      logo.setVelocity(100, 200);
-      logo.setBounce(1, 1);
-      logo.setCollideWorldBounds(true);
-      emitter.startFollow(logo);
-    }
-
-  }
+  scene: Scene
 
 };
 
