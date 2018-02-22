8727dba096e7a48f0b153b1eaf9a61caa68a1de7
samme <samme@users.noreply.github.com>
2018-02-22 14:35:18 -0800

    Convert to ES6 class

--- app/scenes/default.js
+++ app/scenes/default.js
@@ -1,51 +1,51 @@
-module.exports = {
+module.exports = class Default extends Phaser.Scene {
 
-  init: function () {
-    console.log('init', this);
-  },
+  constructor (sceneConfig) {
+    super(sceneConfig);
+  }
 
-  preload: function () {
+  init (data) {
+    console.log('init', data);
+  }
+
+  preload () {
     this.load.image('sky', 'space3.png');
     this.load.image('logo', 'phaser3-logo.png');
     this.load.image('red', 'red.png');
     this.progressBar = this.add.graphics(0, 0);
     this.load.on('progress', this.onLoadProgress, this);
     this.load.on('complete', this.onLoadComplete, this);
-  },
+  }
 
-  create: function () {
-    var sky = this.add.image(400, 300, 'sky');
+  create () {
+    const sky = this.add.image(400, 300, 'sky');
     sky.alpha = 0.5;
-    var particles = this.add.particles('red');
-    var emitter = particles.createEmitter({
+    const particles = this.add.particles('red');
+    const emitter = particles.createEmitter({
       speed: 100,
       scale: { start: 1, end: 0 },
       blendMode: 'ADD'
     });
-    var logo = this.physics.add.image(400, 100, 'logo');
+    const logo = this.physics.add.image(400, 100, 'logo');
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
     logo.setCollideWorldBounds(true);
     emitter.startFollow(logo);
-  },
+  }
 
-  extend: {
+  // extend:
 
-    progressBar: null,
+  onLoadComplete () { // (loader, storageSize, failedSize)
+    console.log('onLoadComplete');
+    this.progressBar.destroy();
+  }
 
-    onLoadComplete: function () { // (loader, storageSize, failedSize)
-      console.log('onLoadComplete');
-      this.progressBar.destroy();
-    },
-
-    onLoadProgress: function (progress) {
-      this.progressBar
-        .clear()
-        .fillStyle(0xffffff, 0.75)
-        .fillRect(0, 0, 800 * progress, 50);
-      console.log('progress', progress);
-    }
-
+  onLoadProgress (progress) {
+    this.progressBar
+      .clear()
+      .fillStyle(0xffffff, 0.75)
+      .fillRect(0, 0, 800 * progress, 50);
+    console.log('progress', progress);
   }
 
 };
