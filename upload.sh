c3b470b5633f16478cb55c9396f99355cf7421f0
Nathan  Aschenbach <nate@gamestartschool.org>
2018-03-03 15:28:55 -0800

    Adding scripts

---
+++ upload.sh
@@ -0,0 +1,6 @@
+npm run build
+
+#macOS - Please make sure itch.io app is installed https://itch.io/app
+export PATH="$PATH:~/Library/Application Support/itch/bin"
+
+butler push public inventonater/roguehack:html
