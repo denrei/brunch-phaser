9ddb1f3903505de1359c4394a46c3c6089dda128
nshiff <nshiff@users.noreply.github.com>
2018-03-03 22:16:34 -0500

    reindent

--- app/scenes/tiledemo.js
+++ app/scenes/tiledemo.js
@@ -1,32 +1,32 @@
 export default class TileDemo extends Phaser.Scene {
 
-  constructor () {
-    super('tiledemo');
-    this.score = null;
-  }
+    constructor() {
+        super('tiledemo');
+        this.score = null;
+    }
 
-  init (data) {
-    console.log('init', this.scene.key, data, this);
-    this.score = 0;
-  }
+    init(data) {
+        console.log('init', this.scene.key, data, this);
+        this.score = 0;
+    }
 
-  create () {
-      this.add.text(400, 300, 'TILE DEMO', {
-          fill: 'white',
-          fontSize: 48
-      })
-          .setOrigin(0.5)
-          .setShadow(0, 1, '#62F6FF', 10);
-  }
+    create() {
+        this.add.text(400, 300, 'TILE DEMO', {
+            fill: 'white',
+            fontSize: 48
+        })
+            .setOrigin(0.5)
+            .setShadow(0, 1, '#62F6FF', 10);
+    }
 
-  update () {
-    // this.score += 1;
-  }
+    update() {
+        // this.score += 1;
+    }
 
-  // extend:
+    // extend:
 
-  quit () {
-    // this.scene.start('menu', { score: this.score });
-  }
+    quit() {
+        // this.scene.start('menu', { score: this.score });
+    }
 
 }
